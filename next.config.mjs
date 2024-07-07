a6225f7d0e8eef158a0ab87cb87de26abe3f8664
Rahul Gupta <rg5353070@gmail.com>
2024-07-07 12:19:45 +0530

    added support for remote image and added react icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,41 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com/**",
+      },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com/**",
+      },
+      {
+        protocol: "https",
+        hostname: "loremflickr.com/**",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cloudflare-ipfs.com/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
